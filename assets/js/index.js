472c5dc0b6522a7ac0e5970b998ab28e80395643
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-04-30 16:54:44 +0100

    Initial JavaScript game logic defined
    - Created variables to target start button, homepage banner section, the main HTML element, and timer element
    
    - Created a questions and answers array
    
    - Created a render question function, which gets the current question by using an empty question index array i declared, then creates elements to match the html structure and css classes and ID's i defined earlier
    
    - Created a function that validates that when the quiz is running, only a list item has been clicked, and then gets the question and answer the user clicked on, and puts them both in an answer object, which i intend to send to local storage
    
    - Created a function that removes the homepage banner section from the page when the user clicks the start button
    
    - Connected all these functions together with a function that gets triggered when the start button is clicked - handleStartButtonClick - this then triggers removeBanner(), renderQuestion() and startGame() which renders the first question and starts the timer

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,21 +5,132 @@
 // let questionIndex = 0;
 // let quizComplete = false;
 
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector("#start-btn");
 
-// The startGame function is called when the start button is clicked
-function startGame() {
+// target start button
+const startButton = document.getElementById("start-btn");
+
+// target banner section
+const bannerSection = document.getElementById("banner");
+
+// target main element
+const mainElement = document.getElementById("main");
+
+// current question index
+let questionIndex = 0;
+
+var timerElement = document.querySelector(".timer-count");
+// var startButton = document.querySelector("#start-btn");
+
+// all options
+// const options = ["Yes", "No", "Maybe"];
+
+// all questions array
+const questions = [
+    {
+      text: "What was the name of Abe Simpsons military unit?",
+      options: ["The Chirping Cheep Cheeps", "The Jumping Sushies", "The Flaming Salamanders", "The Flying Hellfish"]
+    },
+];
+
+// event handler function to handle click events in question section
+// this function decides what happens next
+const handleOptionClick = (event) => {
+    console.log("clicked somewhere in question section");
+  
+    // get current target
+    const currentTarget = event.currentTarget;
+  
+    // get target
+    const target = event.target;
+  
+    // check if click originates from li only
+    // check if target element is li element
+    if (target.tagName === "LI") {
+      // get the option the user clicked on
+      const value = target.getAttribute("data-value");
+  
+      // get the question the user answered
+      const question = questions[questionIndex].text;
+  
+      // build an answer object that contains question and answer
+      const answer = {
+        question,
+        value,
+      };
+    }
+}
+
+
+
+
+
+// function to render question to page
+const renderQuestion = () => {
+    console.log("render question");
+  
+    // get current question
+    const currentQuestion = questions[questionIndex];
+  
+    // create section
+    const section = document.createElement("section");
+    section.setAttribute("class", "content-section question-container");
+    section.setAttribute("id", "question-container");
+  
+    // create h2
+    const h2 = document.createElement("h2");
+    h2.setAttribute("class", "content-section-title");
+    // TODO: this should be the dynamic question title
+    h2.textContent = `${questionIndex + 1}. ${currentQuestion.text}`;
+  
+    // create ul and append 3 li
+    const ul = document.createElement("ul");
+    ul.setAttribute("class", "feedback-list");
+  
+    // TODO: loop over options to create and append li to ul
+    const li1 = document.createElement("li");
+    li1.setAttribute("class", "list-item");
+    li1.setAttribute("data-value", currentQuestion.options[0]);
+    li1.textContent = currentQuestion.options[0];
+  
+    const li2 = document.createElement("li");
+    li2.setAttribute("class", "list-item");
+    li2.setAttribute("data-value", currentQuestion.options[1]);
+    li2.textContent = currentQuestion.options[1];
+  
+    const li3 = document.createElement("li");
+    li3.setAttribute("class", "list-item");
+    li3.setAttribute("data-value", currentQuestion.options[2]);
+    li3.textContent = currentQuestion.options[2];
+  
+    ul.append(li1, li2, li3);
+  
+    // append h2 and ul to section
+    section.append(h2, ul);
+  
+    // append question section to main element
+    mainElement.append(section);
+  
+    // add event listener on question section
+    section.addEventListener("click", handleOptionClick);
+  };
+  
+
+  // function to remove banner from page
+const removeBanner = () => {
+    console.log("remove banner");
+    bannerSection.remove();
+  };
+
+  // The startGame function is called when the start button is clicked
+function startTimer() {
     // isWin = false;
     timerCount = 30;
     // Prevents start button from being clicked when round is in progress
     startButton.disabled = true;
-    // renderBlanks()
-    startTimer()
-  }
 
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-function startTimer() {
+
+//     // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+
     // Sets timer
     timer = setInterval(function() {
       timerCount--;
@@ -45,8 +156,31 @@ function startTimer() {
 
 
 
+
+// declare the event handler function for start button click
+const handleStartButtonClick = () => {
+  console.log("start button clicked");
+  // initialise local storage
+//   initialiseLocalStorage();
+
+  // remove banner section
+  removeBanner();
+
+  // render question
+  renderQuestion();
+
+//   Starts the timer
+  startTimer();
+};
+
+
+
   // Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", handleStartButtonClick);
+
+
+// ****************************************************************
+// pseudocode and notes:
 
 // // when start quiz button is pressed, set question index [0]
 
